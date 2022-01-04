21d429efcbefc9523659d9ad45813548c14d8d89
akinyemi sodiq <mallamsiddiq@gmail.com>
2022-01-04 08:03:57 +0100

    added the text editor

--- blog/views.py
+++ blog/views.py
@@ -6,53 +6,33 @@ from .models import Todo
 
 def home(request):
     todos = Todo.objects.all()
-    context = {
-            'todos':todos
-    }
+    context = { 'todos':todos}
     return render(request, 'home.html', context)
 
 def details(request, id):
     todo = get_object_or_404(Todo, pk=id)
-    context = {
-            'todo':todo,
-          
-    }
+    context = { 'todo':todo, }
     return render(request, 'details.html', context)
-
 def add(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        body = request.POST['body']
-        execution_date = request.POST['execution_date']
-        
-        todo = Todo(title=title, body=body,execution_date=execution_date)
+        todo = Todo(title=request.POST['title'], body=request.POST['body'],execution_date=request.POST['execution_date'])
         todo.save()
-      
         messages.success(request, 'Todo Added : %s' % todo.title)
         return redirect(reverse('todos:home'))
     else:
-        
         return render(request, 'form.html')
-
 def update(request, id):
     todo = get_object_or_404(Todo, pk=id)
     if request.method == 'POST':
-        title = request.POST['title']
-        body = request.POST['body']
-        execution_date = request.POST['execution_date']
-        
-        todo = Todo(title=title, body=body,execution_date=execution_date)
+        todo.title = request.POST['title']
+        todo.body = request.POST['body']
+        todo.execution_date = request.POST['execution_date']
         todo.save()
         messages.success(request, 'Todo Updated : %s' % todo.title)
         return redirect(reverse('todos:detail', args=[ id ]))
     else:
-      
-        context = {
-            'todo':todo,
-           
-        }
+        context = { 'todo':todo, }
         return render(request, 'edit.html', context)
-
 def delete(request, id):
     if (request.method == 'POST'):
         todo = get_object_or_404(Todo, pk=id)
